0d95de59e96e187dd8def329f468fa9b296cf5ae
Oskar Triebe <ourownstory@users.noreply.github.com>
2020-02-28 14:51:10 -0800

    Update bash_aliases.sh

--- bashrc_snippets/bash_aliases.sh
+++ bashrc_snippets/bash_aliases.sh
@@ -8,3 +8,12 @@ alias gcm="git commit -m"
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
+
+# cloud aliases
+alias ssh_azure="ssh ourownstory@xx.xx.xxx.xxx"
+alias tb_azure="tensorboard --logdir=results --host xx.xx.xxx.xxx --port 6006"
+alias ssh_aws="ssh -i "~/../keyfile.pem" ec2-user@xx.xxx.xxx.xxx"
+alias jupyter_aws="ssh -i "~/../keyfile.pem" -L 8888:localhost:8888 ec2-user@xx.xxx.xxx.xxx"
+
+# venv aliases
+alias venvxxx="source ~/venv/xxx/bin/activate"
