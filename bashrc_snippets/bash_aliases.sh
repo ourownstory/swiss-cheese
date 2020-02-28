e3e325775582fd245ef63628c36a8fa568ee8f3a
Oskar Triebe <ourownstory@users.noreply.github.com>
2020-02-28 14:45:18 -0800

    Create bash_aliases.sh

---
+++ bashrc_snippets/bash_aliases.sh
@@ -0,0 +1,10 @@
+# git aliases
+alias gs="git status"
+alias gco="git checkout"
+#alias gca="git commit -am"
+alias gcm="git commit -m"
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
