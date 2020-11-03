2ff93ba4726fc304ff6bf4e0cf084e393978d33b
Oskar Triebe <ourownstory@users.noreply.github.com>
2020-11-03 11:28:59 -0800

    Update basics.sh

--- bashrc_snippets/basics.sh
+++ bashrc_snippets/basics.sh
@@ -1,3 +1,15 @@
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+
 # Alias definitions.
 # You may want to put all your additions into a separate file like
 # ~/.bash_aliases, instead of adding them into .bashrc directly.
@@ -12,4 +24,7 @@ if [ -f ~/.py_mods ]; then
     . ~/.py_mods
 fi
 
-
+# add hacks specific to your device
+if [ -f ~/.surface_hacks ]; then
+    . ~/.surface_hacks
+fi
