46a9457e339e13bc1a6471c4f33c23e823e93194
Oskar Triebe <ourownstory@users.noreply.github.com>
2020-11-03 11:37:48 -0800

    Update and rename basics.sh to bashrc_basics.sh

---
+++ bashrc_snippets/bashrc_basics.sh
@@ -0,0 +1,10 @@
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them into .bashrc directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+# add these lines to .bashrc to do so:
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
